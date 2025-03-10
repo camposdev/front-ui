a7fd649915caf7c5c3e85e2ccd9d4ca68f851040
Felipe Campos <felipecamposdev@gmail.com>
2025-03-10 15:00:32 -0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -24,8 +24,7 @@ export default defineConfig({
     rollupOptions: {
       external: Object.keys(peerDependencies),
       output: {
-        globals: { react: "React", "react-dom": "ReactDOM" },
-        entryFileNames: "main.js",
+        globals: { react: "React", "react-dom": "ReactDOM" }
       },
     },
   },
